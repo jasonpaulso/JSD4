23f4b0ddfd53db0343c77f6007ae301312ac7150
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-09-19 21:00:44 -0700

    formulas homework commit

---
+++ homework/functions/project.js
@@ -0,0 +1,41 @@
+// var color = "red";
+// var age = 25;
+// var color;
+// console.log(color);
+// var color = "blue";
+
+
+// add();
+// taco();
+
+// function taco() {
+//  var type = "carne asada";
+//  var beans = "black beans";
+//  var space = " "
+//  console.log("Your" + space + type + space + "taco" + space + "with" + space + beans + space + "is ready.") 
+// }
+
+// function add(number1, number2) {
+//   return number1 + number2;
+//   // var sum = 5;
+// }
+function add(x, y) {
+  var sum = x + y
+  return sum;
+}
+function subtract(x,y) {
+  var result = x - y;
+  return result
+}
+function times(x,y) {
+  var result = x * y;
+  return result
+}
+function divide(x,y) {
+  var result = x / y;
+  return result
+}
+function remainder(x, y) {
+  var result = x % y;
+  return result
+}
